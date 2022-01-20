ac926c8c2cb985dba82265d3fc643a4e66f7bfcb
jiravvit <jirabbit05@gmail.com>
2022-01-19 23:21:35 -0800

    Initial Setting

---
+++ pcap-test.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <pcap.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+void usage() {
+    printf("syntax: pcap-test <interface>\n");
+    printf("sample: pcap-test wlan0\n");
+}
+
+typedef struct {
+    char* dev_;
+} Param;
+
+Param param  = {
+    .dev_ = NULL
+};
+
+bool parse(Param* param, int argc, char* argv[]) {
+    if (argc != 2) {
+        usage();
+        return false;
+    }
+    param->dev_ = argv[1];
+    return true;
+}
